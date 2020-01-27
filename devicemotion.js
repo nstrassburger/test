89601cf64b1b0f708b01b95d75ecc9f5b5146e4e
nstrassburger <60351537+nstrassburger@users.noreply.github.com>
2020-01-27 16:15:04 +0100

    Update devicemotion.js

--- devicemotion.js
+++ devicemotion.js
@@ -1,5 +1,5 @@
 // Inhalt des Scripts devicemotion.js:
-let maxi = -1;
+//let maxi = -1;
 alert ("hallo");
 if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
@@ -7,10 +7,10 @@ if (window.DeviceMotionEvent) {
     document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
     document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
     document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
-    if (event.acceleration.z > maxi) {
-    maxi = event.acceleration.z;
-    document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
-    }
+   // if (event.acceleration.z > maxi) {
+    //maxi = event.acceleration.z;
+    //document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
+    //}
     });
     } else {
     document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
